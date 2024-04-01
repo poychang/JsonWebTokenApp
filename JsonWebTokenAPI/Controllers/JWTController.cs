53831c2ea464a869c73efa26a94b5b5700a9ed98
Poy Chang <poypost@gmail.com>
2024-04-01 17:25:11 +0800

    update

---
+++ JsonWebTokenAPI/Controllers/JWTController.cs
@@ -0,0 +1,119 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.Security.Claims;
+using System.Text;
+
+namespace JsonWebTokenAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JWTController : ControllerBase
+    {
+        // POST /jwt/login
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Get(LoginModel login)
+        {
+            if (Validate(login))
+            {
+                var expireMinutes = 10;
+                return Ok(new
+                {
+                    App = login.Name,
+                    Token = JwtHelper.GenerateToken(login.Name, expireMinutes),
+                    Create = DateTime.Now,
+                    Expire = DateTime.Now.AddMinutes(expireMinutes),
+                });
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            static bool Validate(LoginModel login) => true;
+        }
+
+        // GET /jwt/decode
+        [Authorize]
+        [HttpGet("decode")]
+        public IActionResult Decode()
+        {
+            var handler = new JsonWebTokenHandler();
+            var token = HttpContext.Request.Headers.Authorization.FirstOrDefault()?.Replace("Bearer ", "");
+            var data = handler.ReadJsonWebToken(token);
+            return Ok(data);
+        }
+
+        // GET /jwt/claims
+        [Authorize]
+        [HttpGet("claims")]
+        public IActionResult GetClaims()
+        {
+            var claims = HttpContext.User.Claims.Select(claim => new { claim.Type, claim.Value });
+            return Ok(claims);
+        }
+
+        // GET /jwt/role
+        [Authorize(Roles = "admin")]
+        [HttpGet("role")]
+        public IActionResult GetRole()
+        {
+            var rule = HttpContext.User.Claims.Where(claim => claim.Type == ClaimTypes.Role).Select(claim => claim.Value);
+            return Ok(rule);
+        }
+    }
+
+    public record LoginModel(string Name, string Key);
+
+    public static class JwtHelper
+    {
+        const string Issuer = "Jwt:Issuer";
+        const string SecureKey = "SECURITY_KEY_SHOULD_ABOVE_16_CHARACTERS";
+        public static string GenerateToken(string userName, int expireMinutes = 10)
+        {
+            // Configuring "Claims" to your JWT Token
+            var claims = new List<Claim>
+            {
+                // In RFC 7519 (Section#4), there are defined 7 built-in Claims, but we mostly use 2 of them
+                //new(JwtRegisteredClaimNames.Iss, "issuer"),
+                new(JwtRegisteredClaimNames.Sub, userName), // User.Identity.Name
+                //new(JwtRegisteredClaimNames.Aud, "The Audience"),
+                //new(JwtRegisteredClaimNames.Exp, DateTimeOffset.UtcNow.AddMinutes(expireMinutes).ToUnixTimeSeconds().ToString()),
+                //new(JwtRegisteredClaimNames.Nbf, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString()),
+                //new(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString()),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // JWT ID
+            };
+
+            // You can define your "roles" to your Claims
+            claims.Add(new Claim(ClaimTypes.Role, "admin"));
+            claims.Add(new Claim(ClaimTypes.Role, "users"));
+            // You can add custom claims as well
+            claims.Add(new Claim("custom", "claim"));
+
+            var claimsIdentity = new ClaimsIdentity(claims);
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecureKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Create SecurityTokenDescriptor
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = Issuer,
+                //Audience = issuer, // Sometimes you don't have to define Audience.
+                //NotBefore = DateTime.Now, // Default is DateTime.Now
+                //IssuedAt = DateTime.Now, // Default is DateTime.Now
+                Subject = claimsIdentity,
+                Expires = DateTime.Now.AddMinutes(expireMinutes),
+                SigningCredentials = credentials
+            };
+
+            // Generate a JWT
+            var handler = new JsonWebTokenHandler();
+            var token = handler.CreateToken(tokenDescriptor);
+
+            return token;
+        }
+    }
+}
--- JsonWebTokenApp/Program.cs
+++ JsonWebTokenApp/Program.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Text.Json;
 
 namespace JsonWebTokenApp
 {
-    internal class Program
+    public class Program
     {
+        const string SecureKey = "SECURITY_KEY_SHOULD_ABOVE_16_CHARACTERS";
+
         static void Main(string[] args)
+        {
+            var token = GenerateToken();
+            Console.WriteLine($"JWT: {token}");
+            Console.WriteLine($"\n----------\n");
+
+            var validation = ValidateToken(token);
+            Console.WriteLine($"JWT validation: {validation}");
+            Console.WriteLine($"\n----------\n");
+
+            var data = JWTDecoder(token);
+            Console.WriteLine($"JWT Decode: {data}");
+
+        }
+
+        static string GenerateToken()
         {
             var handler = new JsonWebTokenHandler();
             var token = handler.CreateToken(new SecurityTokenDescriptor
@@ -15,14 +33,45 @@ namespace JsonWebTokenApp
                 Audience = "https://api.poychang.net",
                 Expires = DateTime.UtcNow.AddMinutes(30),
                 IssuedAt = DateTime.UtcNow,
-                Claims = new Dictionary<string, object> { { "", "" } },
+                Claims = new Dictionary<string, object> { { "role", "admin" } },
                 SigningCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SECURITY_KEY_SHOULD_ABOVE_16_CHARACTERS")),
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecureKey)),
                     SecurityAlgorithms.HmacSha256Signature
                 ),
             });
+            return token;
+        }
 
-            Console.WriteLine($"Token: {token}");
+        static bool ValidateToken(string token)
+        {
+            var handler = new JsonWebTokenHandler();
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidIssuer = "https://jwt.poychang.net",
+                ValidAudience = "https://api.poychang.net",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecureKey)),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+            try
+            {
+                var claimsPrincipal = handler.ValidateTokenAsync(token, validationParameters);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        static string JWTDecoder(string token)
+        {
+            var handler = new JsonWebTokenHandler();
+            var data = handler.ReadJsonWebToken(token);
+            return JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
         }
     }
 }
